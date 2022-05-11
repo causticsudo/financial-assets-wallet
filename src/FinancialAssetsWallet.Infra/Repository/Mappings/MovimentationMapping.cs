6c1c728d9b260ebf2a2e9ed79e669381cecccaeb
matheuzin <matheus.luis.developer@gmail.com>
2022-05-11 02:01:59 -0300

    feature: add some domain components

---
+++ src/FinancialAssetsWallet.Infra/Repository/Mappings/MovimentationMapping.cs
@@ -0,0 +1,15 @@
+﻿using FinancialAssetsWallet.Domain.Model.WalletAggregate.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FinancialAssetsWallet.Infra.Repository.Mappings
+{
+    public class MovimentationMapping : IEntityTypeConfiguration<Movimentation>
+    {
+        public void Configure(EntityTypeBuilder<Movimentation> builder)
+        {
+            builder.ToTable("Movimentation");
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
